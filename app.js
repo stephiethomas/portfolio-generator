376342f6ff72189a55ab25f177a34bec2a88c967
Stephanie Thomas <stephie.thomas@gmail.com>
2022-01-10 21:39:42 -0600

    create questions with node.js inquirer

---
+++ app.js
@@ -0,0 +1,27 @@
+const inquirer = require('inquirer');
+console.log(inquirer)
+
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
+
+
+
+
+
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+.prompt([
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+  }
+])
+.then(answers => console.log(answers));
